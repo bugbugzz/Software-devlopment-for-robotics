589ba0f7bf9e0b643f52795e848c255b3a906919
milou! <m.dezwaan@hotmail.nl>
2026-02-18 18:50:55 +0100

    implemented 2 tests

--- src/MazeSolver.cpp
+++ src/MazeSolver.cpp
@@ -78,7 +78,7 @@ bool MazeSolver::traverse(int row, int col) {
        --------------------------------------------------------- */
     maze[row][col] = 'x';
     printMaze();
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
     /* ---------------------------------------------------------
        3. Check for exit
